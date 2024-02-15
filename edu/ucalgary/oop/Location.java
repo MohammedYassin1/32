f192ce1388e56582aa988fb2ce3f7b33a2ec1eb8
xavierlachance <xavier.lachance@ucalgary.ca>
2024-02-14 19:49:21 -0700

    Location file finished

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -1,36 +1,37 @@
 package edu.ucalgary.oop;
 
+import java.util.ArrayList;
+
 public class Location{
     private String name;
     private String address;
-    private DisasterVictim[] occupants;
-    private Supply[] supplies;
+    private ArrayList<DisasterVictim> occupants;
+    private ArrayList<Supply> supplies;
 
     public Location(String name, String address){
         this.name = name;
         this.address = address;
+        this.occupants = new ArrayList<DisasterVictim>();
+        this.supplies = new ArrayList<Supply>();
     }
     /*setters*/
 
     public void setName(String name){this.name = name;}
     public void setAddress(String address){this.address = address;}
-    public void setOccupants(DisasterVictim[] occupants){this.occupants = occupants;}
-    public void setSupplies(Supply[] supplies){this.supplies = supplies;}
+    public void setOccupants(ArrayList<DisasterVictim> occupants){this.occupants = occupants;}
+    public void setSupplies(ArrayList<Supply> supplies){this.supplies = supplies;}
 
     /*getters */
 
     public String getName(){return this.name;}
     public String getAddress(){return this.address;}
-    public DisasterVictim[] getOccupants(){return this.occupants;}
-    public Supply[] getSupplies(){return this.supplies;}
+    public ArrayList<DisasterVictim> getOccupants(){return this.occupants;}
+    public ArrayList<Supply> getSupplies(){return this.supplies;}
     
     /*functions*/
 
-    public void  addOccupant(DisasterVictim occupant){}
-    public void  removeOccupant(DisasterVictim occupant){}
-    public void addSupply(Supply supply){}
-    public void removeSupply(Supply supply){}
+    public void addOccupant(DisasterVictim occupant){this.occupants.add(occupant);}
+    public void removeOccupant(DisasterVictim occupant){this.occupants.remove(occupant);}
+    public void addSupply(Supply supply){this.supplies.add(supply);}
+    public void removeSupply(Supply supply){this.supplies.remove(supply);}
 }
-
-
-
